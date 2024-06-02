37ac5ffa87713de4c798f011e2cae502cb8c1b16
turks223-freddss <127029577+turks223-freddss@users.noreply.github.com>
2024-06-02 14:16:38 +0800

    Update __init__.py

--- pokemon_computer/website/__init__.py
+++ pokemon_computer/website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Pokemon
+    from .models import User, Pokemon, Region
     
     with app.app_context():
         db.create_all()
